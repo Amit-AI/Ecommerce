19bef68dafa314621289e30030f689544470fbc4
Amit Dubey <dubeyamit219@gmail.com>
2022-09-25 18:02:57 +0530

    added loading Animation

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -24,6 +24,7 @@ const ProductGrid = () => {
         // fetchData();
     }, []);
 
+    // Loading animation
     if (status === STATUS.LOADING) {
         return (
             <div className="loading__wrapper">
